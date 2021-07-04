cce7472b5e2f7edc61ffb922f91146434c22f437
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-04 22:00:20 +0100

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Bruce Gilmour on 2021-07-04.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
